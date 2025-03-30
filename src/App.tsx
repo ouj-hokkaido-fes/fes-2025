b1a945ceb8ea151673ab54eecd823cc2d0500c99
kumori-ame <3dsdsdsds@gmail.com>
2025-03-30 15:19:15 +0900

    commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>放送大学</h1>
+        <h2>北海道学習センター</h2>
+        <h2>文化祭</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: process.env.GITHUB_PAGES  
+  ? "fes-2025"            
+  : "./",       
+  plugins: [react()],
+})
